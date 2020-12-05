3cfbc9c7adc0d0e47bebb61faa734f11893e28c3
Rasmus Buchholdt <rasmus.buchholdt@gmail.com>
2020-12-05 17:47:19 +0100

    Add small delay for playback controls

--- src/app/cards/spotify-control-card/spotify-control-card.component.ts
+++ src/app/cards/spotify-control-card/spotify-control-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { delay, tap } from 'rxjs/operators';
 import { SpotifyPlayback } from 'src/app/models/spotify-playback';
 import { SpotifyService } from 'src/app/services/spotify.service';
 
@@ -10,6 +11,7 @@ import { SpotifyService } from 'src/app/services/spotify.service';
 export class SpotifyControlCardComponent implements OnInit {
 
   currentSong = "Fetching information";
+  isPlaying = false;
   playback: SpotifyPlayback | undefined;
 
   constructor(private spotifyService: SpotifyService) { }
@@ -21,7 +23,8 @@ export class SpotifyControlCardComponent implements OnInit {
   getPlayback(): void {
     this.spotifyService.getPlayback().subscribe(playback => {
       this.playback = playback;
-      this.currentSong = playback.is_playing ? `${playback.item.artists[0].name} - ${playback.item.name}` : 'Not playing anything';
+      this.isPlaying = playback.is_playing;
+      this.currentSong = this.isPlaying ? `${playback.item.artists[0].name} - ${playback.item.name}` : 'Not playing anything';
     });
   }
 
@@ -30,21 +33,22 @@ export class SpotifyControlCardComponent implements OnInit {
   }
 
   togglePlayPause(): void {
-    this.spotifyService.togglePlayPause().toPromise().then(() => {
-      this.getPlayback();
-    });
+    this.spotifyService.togglePlayPause().pipe(
+      tap(() => this.isPlaying = !this.isPlaying),
+      delay(500),
+    ).subscribe(() => this.getPlayback());
   }
 
   next(): void {
-    this.spotifyService.next().toPromise().then(() => {
-      this.getPlayback();
-    });
+    this.spotifyService.next().pipe(
+      delay(500),
+    ).subscribe(() => this.getPlayback());
   }
 
   previous(): void {
-    this.spotifyService.previous().toPromise().then(() => {
-      this.getPlayback();
-    });
+    this.spotifyService.previous().pipe(
+      delay(500),
+    ).subscribe(() => this.getPlayback());
   }
 
   formatVolumeLabel(value: number) {
