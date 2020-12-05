b4ca0edb21445079b86342b2902675bf13b276a0
Rasmus Buchholdt <rasmus.buchholdt@gmail.com>
2020-12-05 20:00:23 +0100

    Add functionality to light control card

--- src/app/cards/light-control-card/light-control-card.component.ts
+++ src/app/cards/light-control-card/light-control-card.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { delay, tap } from 'rxjs/operators';
+import { Light } from 'src/app/models/light';
+import { LightService } from 'src/app/services/light.service';
 
 @Component({
   selector: 'app-light-control-card',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LightControlCardComponent implements OnInit {
 
-  constructor() { }
+  @Input() light: Light | null = null;
+  isOn = false;
+  isSwitchOn = false;
+  status = "Getting status";
+
+  constructor(private lightService: LightService) { }
 
   ngOnInit(): void {
+    if (this.light)
+      this.getLightState(this.light);
   }
 
+  getLightState(light: Light) {
+    this.isOn = (light.state.on && light.state.reachable);
+    this.isSwitchOn = light.state.reachable;
+    this.status = `The switch is ${light.state.reachable ? 'on' : 'off'}`
+  }
+
+  refreshLightState() {
+    if (this.light) {
+      this.lightService.getLight(this.light.id).subscribe(light => {
+        this.getLightState(light);
+      });
+    }
+  }
+
+  toggleLight() {
+    if (this.light) {
+      this.lightService.toggleLight(this.light.id).pipe(
+        tap(() => this.isOn = !this.isOn),
+        delay(500),
+      ).subscribe(() => this.refreshLightState());
+    }
+  }
 }
