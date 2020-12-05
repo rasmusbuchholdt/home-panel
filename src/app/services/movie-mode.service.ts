f74c1c39f72b4037b5bd78ee8822ffc2b67b084a
Rasmus Buchholdt <rasmus.buchholdt@gmail.com>
2020-12-05 20:08:49 +0100

    Add movie mode service

---
+++ src/app/services/movie-mode.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieModeService {
+
+  constructor(private http: HttpClient) { }
+
+  public toggleMovieMode(): Observable<any> {
+    return this.http.get(`${environment.apiUrl}/api/moviemode/toggle`);
+  }
+}
