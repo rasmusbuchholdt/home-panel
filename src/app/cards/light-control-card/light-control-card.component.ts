7cc2254cb513072bf63ca1d4c15ce19debd0ba64
Rasmus Buchholdt <rasmus.buchholdt@gmail.com>
2021-10-24 09:45:12 +0200

    Refresh light state every 10 seconds

--- src/app/cards/light-control-card/light-control-card.component.ts
+++ src/app/cards/light-control-card/light-control-card.component.ts
@@ -8,6 +8,8 @@ import { Light } from 'src/app/models/light';
 import { LightConfig } from 'src/app/models/light-config';
 import { LightService } from 'src/app/services/light.service';
 
+const LIGHT_REFRESH_DELAY_IN_SECONS = 10;
+
 @Component({
   selector: 'app-light-control-card',
   templateUrl: './light-control-card.component.html',
@@ -28,8 +30,8 @@ export class LightControlCardComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    if (this.light)
-      this.getLightState(this.light);
+    if (!this.light) return;
+    this.getLightState(this.light);
   }
 
   getLightState(light: Light) {
@@ -38,47 +40,47 @@ export class LightControlCardComponent implements OnInit {
     this.status = `The switch is ${light.state.reachable ? 'on' : 'off'}`;
     this.saturation = this.normalize(light.state.sat, 1, 254);
     this.brightness = this.normalize(light.state.bri, 1, 254);
+
+    setTimeout(() => {
+      this.refreshLightState();
+    }, LIGHT_REFRESH_DELAY_IN_SECONS);
   }
 
   refreshLightState() {
-    if (this.light) {
-      this.lightService.getLight(this.light.id).pipe(take(1)).subscribe(light => {
-        this.getLightState(light);
-      });
-    }
+    if (!this.light) return;
+    this.lightService.getLight(this.light.id).pipe(take(1)).subscribe(light => {
+      this.getLightState(light);
+    });
   }
 
   toggleLight() {
-    if (this.light) {
-      this.lightService.toggleLight(this.light.id).pipe(
-        take(1),
-        tap(() => this.isOn = !this.isOn),
-        delay(500),
-      ).subscribe(() => this.refreshLightState());
-    }
+    if (!this.light) return;
+    this.lightService.toggleLight(this.light.id).pipe(
+      take(1),
+      tap(() => this.isOn = !this.isOn),
+      delay(500),
+    ).subscribe(() => this.refreshLightState());
   }
 
   editLight() {
-    if (this.light) {
-      console.log(this.light);
-      const dialogRef = this.dialog.open(LightConfigDialogComponent, {
-        width: '250px',
-        data: {
-          id: this.light.id,
-          enabled: this.isOn,
-          rgb: { R: 0, G: 0, B: 0, change: false },
-          saturation: this.saturation,
-          brightness: this.brightness,
-        } as LightConfig
-      });
-      dialogRef.afterClosed().subscribe((result: LightConfig) => {
-        if (result && this.light) {
-          this.lightService.setLight(result).pipe(take(1)).subscribe();
-          this.brightness = result.brightness;
-          this.saturation = result.saturation;
-        }
-      });
-    }
+    if (!this.light) return;
+    const dialogRef = this.dialog.open(LightConfigDialogComponent, {
+      width: '250px',
+      data: {
+        id: this.light.id,
+        enabled: this.isOn,
+        rgb: { R: 0, G: 0, B: 0, change: false },
+        saturation: this.saturation,
+        brightness: this.brightness,
+      } as LightConfig
+    });
+    dialogRef.afterClosed().subscribe((result: LightConfig) => {
+      if (result && this.light) {
+        this.lightService.setLight(result).pipe(take(1)).subscribe();
+        this.brightness = result.brightness;
+        this.saturation = result.saturation;
+      }
+    });
   }
 
   normalize(value: number, min: number, max: number): number {
